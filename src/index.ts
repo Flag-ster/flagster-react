8e8ff28a783d363c2998d5e621a03ccc8e02d689
Maxime Le Duc <maximeld19@gmail.com>
2025-03-10 20:21:48 +0100

    Isolate SSR exports from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { useFlags } from "./hooks/use-flags";
 export { useFlag } from "./hooks/use-flag";
 export { useFlagster, FlagsterProvider } from "./provider";
-export * from "flagster";
+export type { Config, FlagsterState } from "flagster";
+export { Flagster, createFlagster } from "flagster";
 export { FeatureEnabled } from "./components/feature-enabled";
 export { FeatureDisabled } from "./components/feature-disabled";
 export { Feature } from "./components/feature";
---
+++ src/server.ts
@@ -0,0 +1,1 @@
+export { createFlagsterSSR } from "flagster";
